60d198b1905209a01f706c7f687028a46b97a56d
Md Masum Hossain <mdmasumhossain1712@gmail.com>
2025-11-19 13:05:00 +0600

    skill page and modifi skill card

--- src/app/components/SkillCard.jsx
+++ src/app/components/SkillCard.jsx
@@ -12,3 +12,14 @@ function SkillCard({src=""}) {
 }
 
 export default SkillCard;
+
+export const Skill = ({src="", disc}) => {
+  return (
+    <div className='flex flex-col items-center justify-center bg-transparent p-3 rounded-lg hover:shadow-lg hover:shadow-primary/30 transition-all duration-300'>
+        <div className='h-20 w-20 relative mb-2'>
+          <Image src={src} fill alt="skill icon"/>
+        </div>
+        <p className='text-center text-foreground mt-2'>{disc}</p>
+    </div>
+  )
+}
--- src/app/pages/skills/page.js
+++ src/app/pages/skills/page.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Skill } from '@/app/components/SkillCard'
+import skillData from '@/app/utility/skillsLogo'
 
 function Skills() {
   return (
-    <>
-      <h1 className='mt-30'>Skills</h1>
-    </>
+    <div className='mt-15 border-1 rounded-lg p-5'>
+      <h1 className='text-4xl '>My Skills</h1>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
+        {skillData.map((skill, index) => (
+          <Skill 
+            key={index}
+            src={skill.src}
+            disc={skill.disc}
+          />
+        ))}
+      </div>
+    </div >
   )
 }
 
