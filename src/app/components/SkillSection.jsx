d36a75095130051517c41d7bf841f17f16cdf23c
Md Masum Hossain <mdmasumhossain1712@gmail.com>
2026-03-01 21:28:31 +0600

    fixed skill sections bug

--- src/app/components/SkillSection.jsx
+++ src/app/components/SkillSection.jsx
@@ -26,7 +26,7 @@ function SkillSection() {
           ))}
         </MotionWrapper>
       </div>
-      <Link href={"/pages/skills"}>
+      <Link href={"/skills"}>
           <div className='btn-outline w-50 mx-auto mt-6 flex items-center justify-center gap-2'>
             <h3>See All Skill</h3>
             <MotionWrapper type='leftRightPalse'>
